28ef7c23d5253ea3de66857460d844e6fb64c80e
mayank <sismmt09@gmail.com>
2021-01-12 17:37:41 +0530

    proto complete

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # HTML2PDF
 Python application to convert html page to pdf.
+
+
+
+
+
+
+# References
+## Documentation : https://xhtml2pdf.readthedocs.io/en/latest/usage.html
---
+++ converter.py
@@ -0,0 +1,26 @@
+from xhtml2pdf import pisa             # import python module
+
+# Define your data
+source_html = open("./test.html")
+output_filename = "test.pdf"
+
+# Utility function
+def convert_html_to_pdf(source_html, output_filename):
+    # open output file for writing (truncated binary)
+    result_file = open(output_filename, "w+b")
+
+    # convert HTML to PDF
+    pisa_status = pisa.CreatePDF(
+            source_html,                # the HTML to convert
+            dest=result_file)           # file handle to recieve result
+
+    # close output file
+    result_file.close()                 # close output file
+
+    # return False on success and True on errors
+    return pisa_status.err
+
+# Main program
+if __name__ == "__main__":
+    pisa.showLogging()
+    convert_html_to_pdf(source_html, output_filename)
